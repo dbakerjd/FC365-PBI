3ec1fd2da37f57256bca5d7fb5583cde20212605
Marc Torruella <rollwhistler@gmail.com>
2021-07-07 16:03:49 +0200

    going msal

--- npp/src/app/app.module.ts
+++ npp/src/app/app.module.ts
@@ -4,18 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { TeamsService } from './teams.service';
+import { ErrorService } from './services/error.service';
+import { TeamsService } from './services/teams.service';
+import { AuthStartComponent } from './auth/auth-start/auth-start.component';
+import { AuthEndComponent } from './auth/auth-end/auth-end.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    AuthStartComponent,
+    AuthEndComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [TeamsService],
+  providers: [TeamsService, ErrorService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
