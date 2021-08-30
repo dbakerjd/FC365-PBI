347839614b3bde0f36181a4127bdf31d3a82cb43
Albert Floyd <albert@beta.barcelona>
2021-08-30 19:00:40 +0200

    Fix filter by type

--- npp/src/app/filter.pipe.ts
+++ npp/src/app/filter.pipe.ts
@@ -1,22 +1,22 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Opportunity } from './services/sharepoint.service';
 
 @Pipe({
   name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(list: any[], titleFilter?: string, statusFilter?: string, typeFilter?: string, indicationFilter?: number): any {
+  transform(list: Opportunity[], titleFilter?: string, statusFilter?: string, typeFilter?: string, indicationFilter?: number): any {
     if (titleFilter) {
       list = [...list.filter(e => e.Title.search(new RegExp(titleFilter, 'i')) > -1)];
     }
     
     if (statusFilter) {
-      statusFilter = statusFilter.toLocaleLowerCase();
-      list = [...list.filter(e => e.OpportunityStatus.toLocaleLowerCase().indexOf(statusFilter) > -1)];
+      list = [...list.filter(e => e.OpportunityStatus.toLocaleLowerCase().indexOf(statusFilter.toLocaleLowerCase()) > -1)];
     }
     
     if (typeFilter) {
-      list = [...list.filter(e => e.OpportunityTypeId === typeFilter)];
+      list = [...list.filter(e => e.OpportunityTypeId === +typeFilter)];
     }
 
     if (indicationFilter) {
