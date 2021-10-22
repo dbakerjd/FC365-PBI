f3ed0228c76b54cdccaa2f6dbd374b9b17f76e05
David Baker <dbaker@jdforecasting.com>
2021-10-22 16:27:33 +0100

    Dynamic PBI report from licensing API

--- npp/src/app/power-bi/power-bi.component.ts
+++ npp/src/app/power-bi/power-bi.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { LicensingService } from '../services/licensing.service';
+import * as microsoftTeams from "@microsoft/teams-js";
+import { ErrorService } from '../services/error.service';
+
+//const PBIReportFromLicencingAPI = "https://app.powerbi.com/groups/b76b03e1-cdd6-4233-b682-a1d81f25ba04/reports/531cfc4a-9c47-4163-a152-37002ad84d6d/ReportSectionf6c5d750370200b00708"
 
 @Component({
   selector: 'app-power-bi',
@@ -7,10 +11,19 @@ import { LicensingService } from '../services/licensing.service';
   styleUrls: ['./power-bi.component.scss']
 })
 export class PowerBiComponent implements OnInit {
+  constructor(public licensing: LicensingService,
+    private error: ErrorService,) {
 
-  constructor(public licensing: LicensingService) { }
-
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    const reportFromLicencingAPI = this.licensing.license?.PowerBi?.Report
+    const entity = encodeURI("1c4340de-2a85-40e5-8eb0-4f295368978b/Home");
+    const deepLink = `https://teams.microsoft.com/l/entity/${entity}?context={"subEntityId":"${reportFromLicencingAPI}"?action=OpenReport&pbi_source=MSTeams"}`
+    try {
+      microsoftTeams.executeDeepLink(deepLink);
+    } catch (e: any) {
+      this.error.handleError(e);
+    }
+  }
 }
