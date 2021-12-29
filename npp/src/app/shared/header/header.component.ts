d16bd3fbaf71ec52599509527c273f042f9e9157
Albert Floyd <floyd.albert@gmail.com>
2021-12-29 20:00:12 +0100

    Add basic count notification

--- npp/src/app/shared/header/header.component.ts
+++ npp/src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { SharepointService } from 'src/app/services/sharepoint.service';
 import { WorkInProgressService } from 'src/app/services/work-in-progress.service';
 
 @Component({
@@ -8,9 +9,16 @@ import { WorkInProgressService } from 'src/app/services/work-in-progress.service
 })
 export class HeaderComponent implements OnInit {
   @Input() isHome = false;
-  constructor(public jobs: WorkInProgressService) { }
 
-  ngOnInit(): void {
+  public notificationsCounter = 0;
+
+  constructor(
+    public jobs: WorkInProgressService, 
+    private readonly sharepoint: SharepointService
+  ) { }
+
+  async ngOnInit() {
+    this.notificationsCounter = await (await this.sharepoint.getUserNotifications((await this.sharepoint.getCurrentUserInfo()).Id)).length;
   }
 
   goBack() {
