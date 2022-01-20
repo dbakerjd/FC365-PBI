debf4b0ca7b6c81421910810c8a6250f9acb4c65
David Baker <dbaker@jdforecasting.com>
2022-01-20 20:30:39 +0000

    switch to inline

--- npp/src/environments/environment.prod.ts
+++ npp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   ssoRedirectUrl: 'https://nice-beach-021ec5303.azurestaticapps.net/auth-end',
   production: true,
-  isInlineApp: false,
+  isInlineApp: true,
   version: '0.5.0'
 };
