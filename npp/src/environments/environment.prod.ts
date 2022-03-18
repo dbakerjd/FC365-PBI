87420d0bde35bdd5f9723c99e5d0a8f7a62ad294
Marc Torruella <rollwhistler@gmail.com>
2022-03-18 11:15:01 +0100

    updated version number

--- npp/src/environments/environment.prod.ts
+++ npp/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   ssoRedirectUrl: 'https://nice-beach-021ec5303.azurestaticapps.net/auth-end',
   production: true,
   isInlineApp: false,
-  version: '0.5.0',
+  version: '0.5.1',
   contact: {
     name: 'Janssen Team',
     email: 'clientcontact@example.com'
