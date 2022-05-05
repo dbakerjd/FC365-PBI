e01dc554207032c2f49a4e59afad8c762e5990a6
David Baker <dbaker@jdforecasting.com>
2022-05-05 11:37:42 +0100

    Update to environment.prod.ts

--- npp/src/environments/environment.prod.ts
+++ npp/src/environments/environment.prod.ts
@@ -2,11 +2,16 @@ export const environment = {
   ssoRedirectUrl: 'https://agreeable-grass-0c93eec03.1.azurestaticapps.net/auth-end',
   production: true,
   isInlineApp: false,
-  version: '0.5.1',
+  version: '0.5.3',
   contact: {
     name: 'PBI Teams',
     email: 'rheath@jdforecasting.com'
   },
   functionAppUrl: 'https://func-fc365-enterprise-pbi.azurewebsites.net/api/PowerBI',
-  functionAppDomain: 'func-fc365-enterprise-pbi.azurewebsites.net'
+  functionAppDomain: 'func-fc365-enterprise-pbi.azurewebsites.net',
+  hashUserEmails: false,
+  licensingInfo: {
+    entityId : 'FC365-Enterprise-NPP-Dev',
+    teamSiteDomain : 'janddconsulting.sharepoint.com'
+  }
 };
