5ecc0d0320a4ea5b0b0a8aece2a11cb92216c941
David Baker <dbaker@jdforecasting.com>
2022-05-06 17:44:03 +0100

    Update variables

--- npp/src/environments/environment.prod.ts
+++ npp/src/environments/environment.prod.ts
@@ -2,16 +2,12 @@ export const environment = {
   ssoRedirectUrl: 'https://nice-cliff-0f19cff03.1.azurestaticapps.net/auth-end',
   production: true,
   isInlineApp: false,
-  version: '0.5.3',
+  version: '0.5.4',
   contact: {
     name: 'Technical Team',
     email: 'dbaker@jdforecasting.com'
   },
   functionAppUrl: 'https://func-fc365-test.azurewebsites.net/api/PowerBI',
   functionAppDomain: 'func-fc365-test.azurewebsites.net',
-  hashUserEmails: false,
-  licensingInfo: {
-    entityId : 'FC365-Test-NPP',
-    teamSiteDomain : 'janddconsulting.sharepoint.com'
-  }
+  hashUserEmails: false
 };
