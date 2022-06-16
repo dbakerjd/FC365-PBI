6e7f524933bf0a3af8c727b64f72cc3f548e4208
Albert Floyd <floyd.albert@gmail.com>
2022-06-16 16:18:59 +0200

    Change locked file error to warning

--- npp/src/app/services/app/error.service.ts
+++ npp/src/app/services/app/error.service.ts
@@ -13,7 +13,7 @@ export class ErrorService {
     let errorMessage = e.message;
     if(e.status) {
       if (e.status == 403) this.subject.next('unauthorized');
-      else if (e.status === 423) errorMessage = "The file is locked by another user right now. Try again later";
+      else if (e.status === 423) this.toastr.warning("The file is locked by another user right now. Try again later"); return;
     }
     this.toastr.error(errorMessage);
   }
