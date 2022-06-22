d99164b5ecf57e191bab1ba1c0f33bf0aa9c5d8d
Albert Floyd <floyd.albert@gmail.com>
2022-06-22 15:07:14 +0200

    Capital letters StringMapper behavior

--- npp/src/app/services/string-mapper.service.ts
+++ npp/src/app/services/string-mapper.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { StringMapping } from '@shared/models/app-config';
 import { AppDataService } from './app/app-data.service';
 
+
+type capitalLetterParam =  'l' | 'lower' | 'u' | 'upper' | 'f' | 'first' | 't' | 'titlecase'; 
 @Injectable({
   providedIn: 'root'
 })
@@ -17,9 +19,32 @@ export class StringMapperService {
     this.mappingList = await this.appData.getStringMappingItems();
   }
 
-  getString(key: string): string {
+  getString(key: string, capitalLetters: capitalLetterParam = 'titlecase'): string {
     if (!this.mappingList) return key;
-    const mappedString = this.mappingList.find(e => e.Key === key);
-    return mappedString ? mappedString.Title : key;
+    const lowerCaseKey = key.toLocaleLowerCase();
+    const mappedString = this.mappingList.find(e => e.Key.toLocaleLowerCase() === lowerCaseKey);
+    if (mappedString) {
+      switch (capitalLetters) {
+        case 'first':
+        case 'f':
+          return mappedString.Title[0].toLocaleUpperCase() + mappedString.Title.slice(1);
+        case 'upper':
+        case 'u':
+          return mappedString.Title.toLocaleUpperCase();
+        case 'lower':
+        case 'l':
+          return mappedString.Title.toLocaleLowerCase();
+        case 'titlecase':
+        case 't':
+          return this.titleCase(mappedString.Title);
+      }
+    }
+    return key;
+  }
+
+  private titleCase(str: string) {
+    return str.split(' ')
+      .map(w => w[0].toLocaleUpperCase() + w.substring(1).toLocaleLowerCase())
+      .join(' ');
   }
 }
