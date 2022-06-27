81d8e8a222215fe2e9800f0290af8292f688212c
David Baker <dbaker@jdforecasting.com>
2022-06-27 11:12:29 +0100

    Update version
    DevOps 467

--- npp/src/environments/environment.prod.ts
+++ npp/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   ssoRedirectUrl: 'https://nice-cliff-0f19cff03.1.azurestaticapps.net/auth-end',
   production: true,
   isInlineApp: false,
-  version: '0.5.9',
+  version: '0.5.10',
   functionAppUrl: 'https://func-fc365-test.azurewebsites.net/api/PowerBI',
   functionAppDomain: 'func-fc365-test.azurewebsites.net',
   //AAD api scope to use. For multitenant must be preceded by domain, otherwise api://{clientID}/{scope}
