dd35de58db96cc8bb6d4d719cc657146b7c8301a
David Baker <dbaker@jdforecasting.com>
2022-07-12 22:24:10 +0100

    Update to PBI-dev variables

--- npp/src/environments/environment.prod.ts
+++ npp/src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
-  ssoRedirectUrl: 'https://icy-water-0ae577403.1.azurestaticapps.net/auth-end',
+  ssoRedirectUrl: 'https://proud-mud-084a1d203.1.azurestaticapps.net/auth-end',
   production: true,
   isInlineApp: false,
   version: '0.6.3',
-  functionAppUrl: 'https://func-fc365-test.azurewebsites.net/api/PowerBI',
-  functionAppDomain: 'func-fc365-test.azurewebsites.net',
+  functionAppUrl: 'https://func-fc365-pbi-dev.azurewebsites.net/api/PowerBI',
+  functionAppDomain: 'func-fc365-pbi-dev.azurewebsites.net',
   //AAD api scope to use. For multitenant must be preceded by domain, otherwise api://{clientID}/{scope}
-  apiScope: 'https://janddconsulting.onmicrosoft.com/FC365-Test-Inline/user_impersonation',
+  apiScope: 'https://janddconsulting.onmicrosoft.com/FC365-Dev-NPP/user_impersonation',
   //AAD client ID
-  clientID: 'fa5c558e-784f-4950-af4e-7ab724b54808',
+  clientID: '3d632646-870b-439e-81a8-7b726b3539c8',
   //use common for multitenant apps, otherwise use TenantId
   authority: 'common'
 };
--- npp/src/environments/environment.ts
+++ npp/src/environments/environment.ts
@@ -10,9 +10,9 @@ export const environment = {
   functionAppUrl: 'http://localhost:7071/api/PowerBI',
   functionAppDomain: 'localhost',
   //AAD api scope to use. For multitenant must be preceded by domain, otherwise api://{clientID}/{scope}
-  apiScope: 'https://janddconsulting.onmicrosoft.com/FC365-Test-Inline/user_impersonation',
+  apiScope: 'https://janddconsulting.onmicrosoft.com/FC365-Dev-NPP/user_impersonation',
   //AAD client ID
-  clientID: 'fa5c558e-784f-4950-af4e-7ab724b54808',
+  clientID: '3d632646-870b-439e-81a8-7b726b3539c8',
   //use common for multitenant apps, otherwise use TenantId
   authority: 'common'
 };
